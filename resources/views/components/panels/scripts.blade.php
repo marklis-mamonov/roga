345a64ebeab83290fddbf7d9d97ff922c4c48e7d
Мамонов Алексей Анатольевич <mamonov_a_qschool@qsoft.ru>
2022-08-02 09:12:03 +0300

    220797 / Интеграция верстки главной страницы

---
+++ resources/views/components/panels/scripts.blade.php
@@ -0,0 +1,3 @@
+<script src="{{ asset('js/vendor/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('js/vendor/slick.min.js') }}"></script>
+<script src="{{ asset('js/script.js') }}"></script>
---
+++ resources/views/components/panels/styles.blade.php
@@ -0,0 +1,6 @@
+<link href="{{ asset('css/form.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
+<link href="{{ asset('css/base.css') }}" rel="stylesheet">
+<link href="{{ asset('css/inner_page_template_styles.css') }}" rel="stylesheet">
+<link href="{{ asset('css/main_page_template_styles.css') }}" rel="stylesheet">
+<link href="{{ asset('js/vendor/slick.css') }}" rel="stylesheet">
