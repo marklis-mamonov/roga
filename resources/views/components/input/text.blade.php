b0ca51fa97755ad10f417bea3990b78d4d288b9f
Мамонов Алексей Анатольевич <mamonov_a_qschool@qsoft.ru>
2022-08-04 19:07:06 +0300

    220802 / Страница создания новости - Исправление замечаний

--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -1,3 +1,3 @@
-@props(['name','placeholder','value'])
+@props(['name','placeholder','value','errorExists'])
 
-<input id="{{ $name }}" name="{{ $name }}" type="text" value="{{ old($name, $value) }}" class="mt-1 block w-full rounded-md <?= ($errors->has('title')) ? 'border-red-600' : 'border-gray-300' ?> shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Введите заголовок">
+<input id="{{ $name }}" name="{{ $name }}" type="text" value="{{ old($name, $value) }}" class="mt-1 block w-full rounded-md @if ($errorExists) border-red-600 @else border-gray-300 @endif shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Введите заголовок">
--- resources/views/components/input/textarea.blade.php
+++ resources/views/components/input/textarea.blade.php
@@ -1,3 +1,3 @@
-@props(['name','value'])
+@props(['name','value','errorExists'])
 
-<textarea id="{{ $name }}" name="{{ $name }}" class="mt-1 block w-full rounded-md <?= ($errors->has($name)) ? 'border-red-600' : 'border-gray-300' ?> shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="3">{{ old($name, $value) }}</textarea>
+<textarea id="{{ $name }}" name="{{ $name }}" class="mt-1 block w-full rounded-md @if ($errorExists) border-red-600 @else border-gray-300 @endif shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="3">{{ old($name, $value) }}</textarea>
