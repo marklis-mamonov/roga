8b4053746a62b683b457c813b7a916eac939ccd8
Мамонов Алексей Анатольевич <mamonov_a_qschool@qsoft.ru>
2022-08-05 15:05:37 +0300

    220802 / Страница создания новости - Исправление замечаний (ещё раз)

--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -1,3 +1,7 @@
 @props(['name','placeholder','value','errorExists'])
 
-<input id="{{ $name }}" name="{{ $name }}" type="text" value="{{ old($name, $value) }}" class="mt-1 block w-full rounded-md @if ($errorExists) border-red-600 @else border-gray-300 @endif shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Введите заголовок">
+@if ($errorExists)
+    <input id="{{ $name }}" name="{{ $name }}" type="text" value="{{ old($name, $value) }}" class="mt-1 block w-full rounded-md border-red-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Введите заголовок">
+@else
+    <input id="{{ $name }}" name="{{ $name }}" type="text" value="{{ old($name, $value) }}" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Введите заголовок">
+@endif
