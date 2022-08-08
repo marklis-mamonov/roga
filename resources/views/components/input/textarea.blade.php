b1d717e5e1d7e8de2d06290a5961015a1b542c65
Мамонов Алексей Анатольевич <mamonov_a_qschool@qsoft.ru>
2022-08-08 17:01:51 +0300

    220802 / Страница создания новости - Исправление замечаний (и ещё раз)

--- resources/views/components/input/textarea.blade.php
+++ resources/views/components/input/textarea.blade.php
@@ -1,7 +1,7 @@
 @props(['name','value','errorExists'])
 
 @if ($errorExists)
-    <textarea id="{{ $name }}" name="{{ $name }}" class="mt-1 block w-full rounded-md border-red-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="3">{{ old($name, $value) }}</textarea>
+    <textarea id="{{ $name }}" name="{{ $name }}" class="mt-1 block w-full rounded-md border-red-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="3">{{ $value }}</textarea>
 @else
-    <textarea id="{{ $name }}" name="{{ $name }}" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="3">{{ old($name, $value) }}</textarea>
+    <textarea id="{{ $name }}" name="{{ $name }}" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="3">{{ $value }}</textarea>
 @endif
--- resources/views/forms/article.blade.php
+++ resources/views/forms/article.blade.php
@@ -2,17 +2,17 @@
     <div class="grid grid-cols-1 gap-6">
         <div class="block">
             <x-input.group for="title" title="Заголовок новости" :errors="$errors">
-                <x-input.text name="title" placeholder="Введите заголовок" :value="$article->title" :errorExists="$errors->has('title')"/>
+                <x-input.text name="title" placeholder="Введите заголовок" :value="old('title', $article->title)" :errorExists="$errors->has('title')"/>
             </x-input.group>
         </div>
         <div class="block">
             <x-input.group for="description" title="Краткое описание новости">
-                <x-input.textarea name="description" :value="$article->description" :errorExists="$errors->has('description')"/>
+                <x-input.textarea name="description" :value="old('description', $article->description)" :errorExists="$errors->has('description')"/>
             </x-input.group>
         </div>
         <div class="block">
             <x-input.group for="body" title="Детальное описание">
-                <x-input.textarea name="body" :value="$article->body" :errorExists="$errors->has('body')"/>
+                <x-input.textarea name="body" :value="old('body', $article->body)" :errorExists="$errors->has('body')"/>
             </x-input.group>
         </div>
         <div class="block">
